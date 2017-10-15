b2657be3b155c24196cbda25976c376ff882e02c
GAMEABC\wangjiajun01 <Wangjiajun01@bianfeng.com>
2017-10-15 14:59:26 +0800

    init

--- MoNiClient/src/mainContro/MainController.java
+++ MoNiClient/src/mainContro/MainController.java
@@ -1,6 +1,20 @@
 package mainContro;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class MainController {
 
-	
+	public static void main(String[] args) {
+		try {
+			Socket s =new Socket("127.0.0.1",10020);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
